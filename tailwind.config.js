d0a907d48870f961961d9121d8568eafb6e1529d
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-05-27 23:07:42 +0100

    May 27 23:07

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         cardNumberColor: '#B7BDCC',
         lightestBlue: '#535766',
         lightBlue: '#ECF3FE',
+        blueAsh: '#FCFBFE'
       },
     },},
   variants: {},
