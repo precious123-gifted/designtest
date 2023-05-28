9ef24916d464c3455a0a8bc099289caf7cb7939b
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-05-28 11:04:28 +0100

    May 28 11:04

--- src/aceCoinPayBody.tsx
+++ src/aceCoinPayBody.tsx
@@ -206,12 +206,12 @@ useEffect(()=>{
 
   return (
     <AceStyle>
-    <div ref={aceRef} className='body h-screen w-screen grid place-items-center'>
+    <div ref={aceRef} className='body h-screen  portrait:h-[370vw]  w-screen grid place-items-center'>
 
 <div className="content bg-backgroundColor h-[40vw] portrait:h-[97%] w-[75%]  portrait:w-[90vw]  portrait:flex-col flex justify-between items-center ">
 
-<div className="section1  w-[65%] portrait:w-[100%] portrait:mb-[50vw] h-full flex flex-col justify-between">
-<div className='headSection w-full h-[10%]  portrait:mb-[10vw]  portrait:sm:mb-[12vw] flex justify-between items-center'>  <div className="logoDiv flex items-center"><img src={aceCoinLogo} alt="" className='w-[11vw] portrait:w-[40vw] object-contain  aspect-[4/3] '  /></div>   <div className="numberDiv text-[1.2vw] portrait:text-[5vw] text-backgroundColor flex items-center"><div className="leftNumbers flex"><span className='grid mr-[0.2vw]  portrait:mr-[1vw]  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>0</span><span className='grid    place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>1</span></div><span className='text-navyBlue   ml-[1vw] mr-[1vw] '>:</span><div className="rightNumbers flex"><span className='grid  mr-[0.2vw] portrait:mr-[1vw]  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>1</span><span className='grid  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>9</span></div></div>  </div>
+<div className="section1  w-[65%] portrait:w-[100%] portrait:mb-[30vw] h-full flex flex-col justify-between">
+<div className='headSection w-full h-[10%]  portrait:mb-[6vw]  portrait:sm:mb-[8vw] flex justify-between items-center'>  <div className="logoDiv flex items-center"><img src={aceCoinLogo} alt="" className='w-[11vw] portrait:w-[40vw] object-contain  aspect-[4/3] '  /></div>   <div className="numberDiv text-[1.2vw] portrait:text-[5vw] text-backgroundColor flex items-center"><div className="leftNumbers flex"><span className='grid mr-[0.2vw]  portrait:mr-[1vw]  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>0</span><span className='grid    place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>1</span></div><span className='text-navyBlue   ml-[1vw] mr-[1vw] '>:</span><div className="rightNumbers flex"><span className='grid  mr-[0.2vw] portrait:mr-[1vw]  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>1</span><span className='grid  place-items-center h-[2.5vw] portrait:h-[8vw] w-[1.9vw] portrait:w-[5vw] rounded-[0.3vw] portrait:rounded-[0.5vw]  bg-navyBlue'>9</span></div></div>  </div>
 <div className='cardNumberSection w-full h-[20%]  portrait:h-[13%]   portrait:mb-[6vw] flex flex-col justify-between '>
 <div className="cardNumberSection-1 w-full h-[68%] portrait:h-[40%] flex  justify-between items-center"><div className="cardNumberTextDiv h-full flex flex-col justify-between"><div className="heading text-navyBlue text-[1.2vw] portrait:text-[4vw] font-semibold">Card Number</div><div className="enterCardNumber text-lightestBlue text-[1vw]  portrait:text-[3.2vw]">Enter the 16-digit card number on the card</div></div> <div className="editButtonDiv flex "><span className='mr-[0.6vw]'><img ref={editIcon} src={Pencil} alt="" className='cursor-pointer w-[2vw]  portrait:w-[6vw] object-contain  aspect-[4/3]'/></span><span ref={editText} className=' cursor-pointer text-customBlue text-[1.2vw]  portrait:text-[3.8vw]'>Edit</span></div></div>
 
@@ -245,7 +245,7 @@ useEffect(()=>{
 
 </div>
 
-<div className="section2  w-[30%] h-full flex flex-col justify-between items-center">
+<div className="section2  w-[30%] portrait:w-full h-full flex flex-col justify-between items-center">
 <div className="topLayer rounded-[0.2vw] h-[6%] w-[20%] bg-customBlue">
 
 </div>
@@ -258,17 +258,17 @@ useEffect(()=>{
  <div className="backgroundDiv relative w-[87%] h-full overflow-hidden flex flex-col justify-between items-center"><img src={radalBG} alt=""  className='max-w-[200%] mt-[25%] absolute '/>
 
 <div className="contentDiv h-full w-full flex flex-col justify-between absolute">
-<div className="chipAndWifiDiv  h-[22%] w-full flex justify-between items-center mt-[2vw]"> <img src={chip} alt="" className='w-[2.4vw] object-contain  aspect-[1/3]'/> <div className="wifi"><img src={wifi} alt="" className='w-[2.5vw] object-contain  aspect-[4/3]'/></div> </div>
+<div className="chipAndWifiDiv  h-[22%] w-full flex justify-between items-center mt-[2vw] portrait:mt-[6vw]"> <img src={chip} alt="" className='w-[2.4vw] portrait:w-[10vw] object-contain  aspect-[1/3]'/> <div className="wifi"><img src={wifi} alt="" className='w-[2.5vw]  portrait:w-[10vw] object-contain  aspect-[4/3]'/></div> </div>
 
 <div className="cardDetailsDiv flex flex-col justify-between h-[45%] w-full">
 
 <div className='cardNameAndNumberSection flex flex-col justify-between h-[30%] w-full'>
-  <div className='nameDiv text-navyBlue text-[1.2vw] '>Jonathan Michael</div>
-<div className='cardNumberDiv text-navyBlue text-[1.3vw] font-semibold flex items-center '> <div className="dotDiv flex items-start mb-[1.2vw]"><span className='mr-[0.4vw] text-[2vw] '>.</span><span className='mr-[0.4vw]  text-[2vw]'>.</span><span className='mr-[0.4vw]  text-[2vw]'>.</span><span className='mr-[1.2vw]  text-[2vw]'>.</span> </div> <span>2490</span></div>
+  <div className='nameDiv text-navyBlue text-[1.2vw]  portrait:text-[4vw] '>Jonathan Michael</div>
+<div className='cardNumberDiv text-navyBlue text-[1.3vw]  portrait:text-[4vw] font-semibold flex items-center '> <div className="dotDiv flex items-start mb-[1.2vw] portrait:mb-[3.5vw] portrait:sm:mb-[4vw]"><span className='mr-[0.4vw] text-[2vw] portrait:text-[6vw]  '>.</span><span className='mr-[0.4vw]  text-[2vw] portrait:text-[6vw] '>.</span><span className='mr-[0.4vw]  text-[2vw] portrait:text-[6vw] '>.</span><span className='mr-[1.2vw]  text-[2vw] portrait:text-[6vw] '>.</span> </div> <span>2490</span></div>
 </div>
 
 <div className="expiryDateAndLogoSection flex justify-between h-[33%]">
-<div className="expiryDateDiv  text-navyBlue text-[1.1vw] font-[450]">09/22</div>  <div className="masterCardLogoDiv"><img src={masterCardLogo} alt="" className='w-[3vw] object-contain  aspect-[4/3]'/></div>
+<div className="expiryDateDiv  text-navyBlue text-[1.1vw]  portrait:text-[3.7vw] font-[450]">09/22</div>  <div className="masterCardLogoDiv"><img src={masterCardLogo} alt="" className='w-[3vw]  portrait:w-[10vw] object-contain  aspect-[4/3]'/></div>
 
 
 </div>
@@ -287,21 +287,21 @@ useEffect(()=>{
 
 
 <div className="productDetailsDiv absolute w-[70%] h-[27%] top-[50%] flex flex-col justify-between ">
-<div className='company w-full flex justify-between '> <div className="title text-lightestBlue text-[1.1vw]">Company</div>  <div className="attachment flex text-navyBlue text-[1.1vw] font-semibold"><img src={appleIcon} alt=""  className='mr-[0.4vw] w-[1.5vw] object-contain  aspect-[4/3]'/><span>Apple</span></div> </div>
-<div className='orderNumber  flex justify-between'><div className="title text-lightestBlue text-[1.1vw]">Order Number</div>  <div className="attachment text-navyBlue text-[1.1vw]  font-semibold">3467322</div> </div>
-<div className='product  flex justify-between'><div className="title text-lightestBlue text-[1.1vw]">Product</div>  <div className="attachment text-navyBlue text-[1.1vw] font-semibold ">Macbook Air</div> </div>
-<div className='vat  flex justify-between'><div className="title text-lightestBlue text-[1.1vw]">VAT(20%)</div>  <div className="attachment text-navyBlue text-[1.1vw] font-semibold ">$100.00</div> </div>
+<div className='company w-full flex justify-between '> <div className="title text-lightestBlue text-[1.1vw] portrait:text-[4vw]">Company</div>  <div className="attachment flex text-navyBlue text-[1.1vw] portrait:text-[4vw] font-semibold"><img src={appleIcon} alt=""  className='mr-[0.4vw] w-[1.5vw] portrait:w-[6vw] object-contain  aspect-[4/3]'/><span>Apple</span></div> </div>
+<div className='orderNumber  flex justify-between'><div className="title text-lightestBlue text-[1.1vw] portrait:text-[4vw]">Order Number</div>  <div className="attachment text-navyBlue text-[1.1vw] portrait:text-[4vw] font-semibold">3467322</div> </div>
+<div className='product  flex justify-between'><div className="title text-lightestBlue text-[1.1vw] portrait:text-[4vw]">Product</div>  <div className="attachment text-navyBlue text-[1.1vw] portrait:text-[4vw] font-semibold ">Macbook Air</div> </div>
+<div className='vat  flex justify-between'><div className="title text-lightestBlue text-[1.1vw] portrait:text-[4vw]">VAT(20%)</div>  <div className="attachment text-navyBlue text-[1.1vw] portrait:text-[4vw] font-semibold ">$100.00</div> </div>
 
 </div>
 
-<div className="dottedSection absolute top-[77%] w-full  flex justify-between items-center "><div className="circle rounded-full bg-backgroundColor w-[2vw] h-[2vw] ml-[-5%]"></div><div className="dash w-full  h-[0.2vw] border-navyBlue "></div><div className="circle mr-[-5%] rounded-full bg-backgroundColor w-[2vw] h-[2vw]"></div></div>
+<div className="dottedSection absolute top-[77%] portrait:top-[80%] w-full  flex justify-between items-center "><div className="circle rounded-full bg-backgroundColor w-[2vw] h-[2vw] ml-[-5%]"></div><div className="dash w-full  h-[0.2vw] border-navyBlue "></div><div className="circle mr-[-5%] rounded-full bg-backgroundColor w-[2vw] h-[2vw]"></div></div>
 
 <div className="extraPaymentDiv absolute flex justify-between items-center h-[12%] w-[70%] top-[85%]">   
 <div className="writeUpAndNumberSection flex flex-col justify-between">   
-<div className="writeUp text-lightestBlue text-[1.1vw]">You have to pay</div>
-<div className="number"><span className='text-navyBlue text-[1.8vw] font-semibold'>$599</span><span className='text-navyBlue text-[1.1vw] font-[400]'>.</span><span className='text-navyBlue text-[1.1vw] font-[400]'>99</span></div>
+<div className="writeUp text-lightestBlue text-[1.1vw] portrait:text-[4vw]">You have to pay</div>
+<div className="number"><span className='text-navyBlue text-[1.8vw] portrait:text-[5vw] font-semibold'>$599</span><span className='text-navyBlue text-[1.1vw] portrait:text-[4vw] font-[400]'>.</span><span className='text-navyBlue text-[1.1vw] portrait:text-[4vw] font-[400]'>99</span></div>
 </div> 
- <div className="icon"><img src={receipt} alt=""  className='mr-[0.4vw] w-[3vw] object-contain  aspect-[4/3]'/></div>
+ <div className="icon"><img src={receipt} alt=""  className='mr-[0.4vw] w-[3vw] portrait:w-[10vw] object-contain  aspect-[4/3]'/></div>
 
 </div>
 
